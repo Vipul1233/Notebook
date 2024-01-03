939891a05fbe031a32ca49bf054f653c80327100
vipulSoal <vipulkumaragain@gmail.com>
2024-01-03 17:42:20 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express= require("express");
+const notes=require("./data/node")
+const dotenv=require("dotenv")
+const app =express();
+dotenv.config();
+
+app.get("/",(req,res)=>{
+    res.send("api is running");
+})
+
+app.get("/api",(req,res)=>{
+    res.json(notes)
+})
+
+app.get("/api/:id",(req,res)=>{
+        const note=notes.find((n)=>n._id===req.params.id);
+        res.json(note);
+})
+
+const PORT=process.env.PORT||5000;
+
+app.listen(PORT,()=>{
+    console.log(`server started at port no ${PORT}`);
+})
+
+
+
+
+
